c8d7d9e8f7f819aa762dcb80ecbf8f61f6c74e4a
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2019-03-20 12:12:01 +0100

    [CR] Context comments

--- v2/plugin/context.go
+++ v2/plugin/context.go
@@ -1,17 +1,32 @@
 package plugin
 
+// Context provides metric and configuration API to be used by custom code.
 type Context interface {
+	// Returns configuration value by providing path (representing its position in JSON tree)
 	Config(string) (string, bool)
+
+	// Returns list of allowed configuration paths
 	ConfigKeys() []string
+
+	// Return raw configuration (JSON string)
 	RawConfig() string
 
+	// Store any object between Collect requests using key
 	Store(string, interface{})
+
+	// Load stored object between Collect requests using key
 	Load(string) (interface{}, bool)
 
+	// Add concrete metric with calculated value
 	AddMetric(Namespace, MetricValue) error
+
+	// Add concrete metric with calculated value and tags
 	AddMetricWithTags(Namespace, MetricValue, Tags) error
 
+	// Add tags to specific metric
 	ApplyTagsByPath(Namespace, Tags) error
+
+	// Add tags to all metrics matching regular expression
 	ApplyTagsByRegExp(Namespace, Tags) error
 }
 
