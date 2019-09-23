44cf4bc28dd42b64038950ed5ad1f9491f2e6819
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2019-09-23 17:00:14 +0200

    PluginRPC cleaning

--- v2/internal/pluginrpc/proxies.go
+++ v2/internal/pluginrpc/proxies.go
@@ -2,7 +2,12 @@ package pluginrpc
 
 import "github.com/librato/snap-plugin-lib-go/v2/internal/util/types"
 
-type Publisher interface {
+type CollectorProxy interface {
+	RequestCollect(id string) ([]*types.Metric, error)
+	LoadTask(id string, rawConfig []byte, mtsSelectors []string) error
+	UnloadTask(id string) error
+}
+type PublisherProxy interface {
 	RequestPublish(id string, mts []*types.Metric) error
 	LoadTask(id string, config []byte) error
 	UnloadTask(id string) error
