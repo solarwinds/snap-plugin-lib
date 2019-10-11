4007a4f91870db1298ee4ca117a5f87fc3f6f30c
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2019-10-11 14:16:04 +0200

    Function arguments names
    JIRA: AO-14355

--- v2/plugin/context.go
+++ v2/plugin/context.go
@@ -3,7 +3,7 @@ package plugin
 // CollectContext provides state and configuration API to be used by custom code.
 type Context interface {
 	// Returns configuration value by providing path (representing its position in JSON tree)
-	Config(string) (string, bool)
+	Config(key string) (string, bool)
 
 	// Returns list of allowed configuration paths
 	ConfigKeys() []string
@@ -12,8 +12,8 @@ type Context interface {
 	RawConfig() []byte
 
 	// Store any object between Collect requests using key
-	Store(string, interface{})
+	Store(key string, value interface{})
 
 	// Load stored object between Collect requests using key
-	Load(string) (interface{}, bool)
+	Load(key string) (interface{}, bool)
 }
