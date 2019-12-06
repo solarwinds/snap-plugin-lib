f2b821a4411844ec065d3b9e80579a2604861ac6
Adamik Lukasz <38193046+adamiklukasz@users.noreply.github.com>
2019-12-06 14:25:31 +0100

    Default limit should be set to 0 (#37)
    JIRA: AO-14967

--- v2/plugin/const.go
+++ v2/plugin/const.go
@@ -1,5 +1,5 @@
 package plugin
 
 const (
-	NoLimit = -1 // special value indicating no limits (number of instances or tasks)
+	NoLimit = 0 // special value indicating no limits (number of instances or tasks)
 )
