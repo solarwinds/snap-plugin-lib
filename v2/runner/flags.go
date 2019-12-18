2399198d13333d9f2b8e6e219704ff6f3afcf176
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2019-12-18 14:31:01 +0100

    Enabling command line options for Py

--- v2/runner/flags.go
+++ v2/runner/flags.go
@@ -155,7 +155,9 @@ func ParseCmdLineOptions(pluginName string, pluginType types.PluginType, args []
 
 	flagParser := newFlagParser(pluginName, pluginType, opt)
 
-	err := flagParser.Parse(args)
+	fmt.Printf("args = %s", args)
+
+	err := flagParser.Parse(args[1:]) // todo: for python
 	if err != nil {
 		return opt, fmt.Errorf("can't parse command line options: %v", err)
 	}
