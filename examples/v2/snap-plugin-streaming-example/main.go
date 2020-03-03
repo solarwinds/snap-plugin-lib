38c418bf31dbe4b1df9b5c1c28b3fe10e2357386
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-03-03 16:31:47 +0100

    Streaming simple example
    JIRA: 15733

--- examples/v2/snap-plugin-streaming-example/main.go
+++ examples/v2/snap-plugin-streaming-example/main.go
@@ -4,11 +4,15 @@ import (
 	"github.com/librato/snap-plugin-lib-go/v2/plugin"
 	"github.com/librato/snap-plugin-lib-go/v2/runner"
 	"github.com/sirupsen/logrus"
+	"math/rand"
+	"time"
 )
 
 const (
 	pluginName    = "example-streaming"
 	pluginVersion = "0.0.1"
+
+	maxProbeDuration = 10 * time.Second
 )
 
 var log = logrus.WithFields(logrus.Fields{
@@ -17,10 +21,17 @@ var log = logrus.WithFields(logrus.Fields{
 })
 
 type streamCollector struct {
+	probeID int
 }
 
 func (c *streamCollector) StreamingCollect(ctx plugin.CollectContext) {
+	log.Trace("StreamingCollect start")
 
+	c.probeID++
+	_ = ctx.AddMetric("/stream/probes/id", c.probeID)
+
+	waitDuration := time.Duration(rand.Intn(int(maxProbeDuration)))
+	time.Sleep(waitDuration)
 }
 
 func main() {
