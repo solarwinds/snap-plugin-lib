8fd1ed25c4cf0a415de2f9b7aa5240927f0c293e
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-03-12 15:52:12 +0100

    [CR] inProc

--- v2/runner/inprocess.go
+++ v2/runner/inprocess.go
@@ -10,6 +10,10 @@ func StartCollectorInProcess(collector plugin.InProcessCollector, opt *plugin.Op
 	startCollector(types.NewCollector(collector.Name(), collector.Version(), collector), opt, grpcChan)
 }
 
+func StartStreamingCollectorInProcess(collector plugin.InProcessStreamingCollector, opt *plugin.Options, grpChan chan<- grpchan.Channel) {
+	startCollector(types.NewStreamingCollector(collector.Name(), collector.Version(), collector), opt, grpChan)
+}
+
 func StartPublisherInProcess(publisher plugin.InProcessPublisher, opt *plugin.Options, grpcChan chan<- grpchan.Channel) {
 	startPublisher(publisher, publisher.Name(), publisher.Version(), opt, grpcChan)
 }
