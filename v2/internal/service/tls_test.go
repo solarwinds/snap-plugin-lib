0d39b98ba8742169f25652536afae1e1df1e3cbc
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-04-03 13:42:26 +0200

    Refactor medium test
    JIRA: AO-16028

--- v2/internal/service/tls_test.go
+++ v2/internal/service/tls_test.go
@@ -1,13 +1,10 @@
-// +build small
+// +build medium
 
 package service
 
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 	"io/ioutil"
 	"net"
 	"path/filepath"
@@ -15,45 +12,93 @@ import (
 	"time"
 
 	"github.com/librato/snap-plugin-lib-go/v2/plugin"
+	"github.com/librato/snap-plugin-lib-go/v2/pluginrpc"
 	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+///////////////////////////////////////////////////////////////////////////////
+
+type controlMock struct {
+	closeCh chan bool
+}
+
+func (c *controlMock) Ping(ctx context.Context, request *pluginrpc.PingRequest) (*pluginrpc.PingResponse, error) {
+	return &pluginrpc.PingResponse{}, nil
+}
+
+func (c *controlMock) Kill(ctx context.Context, request *pluginrpc.KillRequest) (*pluginrpc.KillResponse, error) {
+	close(c.closeCh)
+	return &pluginrpc.KillResponse{}, nil
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+const (
+	certificateFolderName = "cert_test"
+	grpcPort              = 50123
+	grpcServerAddr        = "localhost:50123"
+
+	tlsTestTimeout = 10 * time.Second
 )
 
 func TestConnectingToSecureGRPC(t *testing.T) {
 	Convey("Validate that client can connect to secure GRPC Server", t, func() {
 		// Arrange
 		opt := &plugin.Options{
+			GRPCPort:    grpcPort,
 			EnableTLS:   true,
-			TLSKeyPath:  filepath.Join("cert_test", "serv.key"),
-			TLSCertPath: filepath.Join("cert_test", "serv.crt"),
+			TLSKeyPath:  filepath.Join(certificateFolderName, "serv.key"),
+			TLSCertPath: filepath.Join(certificateFolderName, "serv.crt"),
 		}
 
-		var ln net.Listener
+		ln, _ := net.Listen("tcp", grpcServerAddr)
+
+		controlService := &controlMock{closeCh: make(chan bool)}
 
 		// Arrange (GRPC Server)
 		go func() {
 			srv, _ := NewGRPCServer(opt)
+			pluginrpc.RegisterControllerServer(srv.(*grpc.Server), controlService)
 
-			ln, _ = net.Listen("tcp", "")
+			go func() {
+				<-controlService.closeCh
+				srv.Stop()
+			}()
+
 			_ = srv.Serve(ln)
 		}()
 
-		time.Sleep(2 * time.Second)
-
 		// Arrange (GRPC Client)
-		caCertPath := filepath.Join("cert_test", "ca.crt")
+		caCertPath := filepath.Join(certificateFolderName, "ca.crt")
 		caCert, _ := ioutil.ReadFile(caCertPath)
 		certPool := x509.NewCertPool()
 		certPool.AppendCertsFromPEM(caCert)
 
 		creds := credentials.NewTLS(&tls.Config{
 			InsecureSkipVerify: false,
-			RootCAs: certPool,
+			RootCAs:            certPool,
 		})
-		conn, err := grpc.Dial(ln.Addr().String(), grpc.WithTransportCredentials(creds))
-		fmt.Printf("conn %v\n", conn)
-		fmt.Printf("err %v\n", err)
 
-		time.Sleep(10 * time.Second)
+		// Act
+		conn, dialErr := grpc.Dial(grpcServerAddr, grpc.WithTransportCredentials(creds))
+		cc := pluginrpc.NewControllerClient(conn)
 
+		_, errPing := cc.Ping(context.Background(), &pluginrpc.PingRequest{})
+		_, errKill := cc.Kill(context.Background(), &pluginrpc.KillRequest{})
+
+		// Assert
+		So(dialErr, ShouldBeNil)
+		So(errPing, ShouldBeNil)
+		So(errKill, ShouldBeNil)
+
+		select {
+		case <-controlService.closeCh:
+		// ok
+		case <-time.After(tlsTestTimeout):
+			panic("timeout occurred, kill wasn't properly received")
+		}
 	})
 }
