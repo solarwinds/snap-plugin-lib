9c91e8baa8d841bc63ec9ef7a94918f3e508e7a7
Dominik Maslyk <dominik.maslyk@solarwinds.com>
2020-08-06 09:42:54 +0200

    Dev

--- v2/plugin-lib/snap-publisher-plugin-lib-example.py
+++ v2/plugin-lib/snap-publisher-plugin-lib-example.py
@@ -10,22 +10,17 @@ class ExamplePublisherPlugin(BasePublisher):
         pass
 
     def publish(self, ctx):
-        ctx.log(LOGLEVEL_INFO, "Plugin is publishing", {
-            "name": "py-example"
-        })
+        ctx.log(LOGLEVEL_INFO, "Plugin is publishing", {"name": self._name})
         pprint.pprint("Number of received metrics: {}".format(ctx.count()))
         pprint.pprint(ctx.list_all_metrics())
 
     def load(self, ctx):
-        ctx.log(LOGLEVEL_INFO, "Plugin is being loaded", {
-            "name": "py-example"
-        })
+        ctx.log(LOGLEVEL_INFO, "Plugin is being loaded", {"name": self._name})
 
     def unload(self, ctx):
-        ctx.log(LOGLEVEL_INFO, "Plugin is being unloaded", {
-            "name": "py-example"
-        })
+        ctx.log(LOGLEVEL_INFO, "Plugin is being unloaded", {"name": self._name})
 
-if __name__ == '__main__':
-    publisher = ExamplePublisherPlugin("example", "0.0.1")
+
+if __name__ == "__main__":
+    publisher = ExamplePublisherPlugin("publisher-example", "0.0.1")
     start_publisher(publisher)
