3c971d4827eabd22697c2cd42326ba392c1fd47f
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-08-18 15:56:48 +0200

    Cleaning

--- scripts/common.sh
+++ scripts/common.sh
@@ -115,11 +115,9 @@ _copyrights() {
   copyright_error=0
   for f in $(_test_files); 
   do 
-    # echo "Testing $f"
-    lastmod=$(git log -1 --pretty="format:%ci" $f)
-    year=${lastmod:0:4}
-    # grep -q "Copyright (c) ${year} SolarWinds Worldwide, LLC" $f | tee /dev/stderr || echo "ERROR: Wrong copyright header: $f" && false
-    grep -q "Copyright (c) ${year} SolarWinds Worldwide, LLC" $f | tee /dev/stderr || echo "ERROR: Wrong copyright header: $f" && copyright_error=1
+    last_mod_time=$(git log -1 --pretty="format:%ci" $f)
+    year=${last_mod_time:0:4}
+    grep -q "Copyright (c) ${year} SolarWinds Worldwide, LLC" $f | tee /dev/stderr || (echo "ERROR: Wrong copyright header: $f" && copyright_error=1)
   done;
 
   if [[ $copyright_error -eq 1 ]]; then echo "Wrong copyrights"; false; fi
