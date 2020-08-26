2909f7c9b4e841ca251bd35a16c40b24a8229ab4
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-08-26 17:04:39 +0200

    Generating pb files
    JIRA: AO-17180

--- v2/pluginrpc/gen-proto.sh
+++ v2/pluginrpc/gen-proto.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+proto_name="plugin_v2"
 protoc_ver="3.13.0"
+protoc_gen_go_ver="1.2.0"
 
 licence='/*
 Copyright (c) '"$(date +"%Y")"' SolarWinds Worldwide, LLC
@@ -18,21 +20,42 @@ Copyright (c) '"$(date +"%Y")"' SolarWinds Worldwide, LLC
     limitations under the License.
 */'
 
-if ! which protoca &> /dev/null ; then
-    if [[ $OS == "Windows_NT" ]]; then
-        protoc_os="win64"
-        protoc_ext=".exe"
-    else
-        protoc_os="linux-x86_64"
-        protoc_ext=""
-    fi
+linter_ignore='//lint:file-ignore SA1019 Ignore deprecated. Should be fixed by protoc generator.'
 
+if [[ $OS == "Windows_NT" ]]; then
+    protoc_os="win64"
+    ext=".exe"
+else
+    protoc_os="linux-x86_64"
+    ext=""
+fi
+
+echo "Checking for protoc"
+if ! which "protoc${ext}" &> /dev/null ; then
     protoc_path="https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_ver}/protoc-${protoc_ver}-${protoc_os}.zip"
     
     echo "Installing protoc from ${protoc_path}"
     curl -o protoc.zip -L $protoc_path -s
-    unzip -p protoc.zip "bin/protoc${protoc_ext}" > "protoc${protoc_ext}"
-    mv "protoc${protoc_ext}" $GOPATH/bin
+    unzip -p protoc.zip "bin/protoc${ext}" > "protoc${ext}"
+    mv "protoc${ext}" $GOPATH/bin
 fi
 
+echo "Checking for protoc-gen-go"
+if ! which "protoc-gen-go${ext}" &> /dev/null ; then
+    echo "Installing protoc-gen-go"
+    go get github.com/golang/protobuf/protoc-gen-go
+fi
+
+echo "Checking for protoc-gen-grpchan"
+if ! which "protoc-gen-grpchan${ext}" &> /dev/null ; then
+    echo "Installing protoc-gen-grpchan"
+    GO111MODULE=on get "github.com/golang/protobuf/protoc-gen-go@v${protoc_gen_go_ver}"
+fi
+
+echo "Generating pb.go files"
+protoc --go_out=plugins=grpc:. --grpchan_out=. "${proto_name}".proto
+
+echo "Applying licence and linter entry"
+echo -e "${licence}\n${linter_ignore}\n"  | cat - "${proto_name}.pb.go" > temp && mv temp "${proto_name}.pb.go"
+echo -e "${licence}\n" | cat - "${proto_name}.pb.grpchan.go" > temp && mv temp "${proto_name}.pb.grpchan.go"
 
