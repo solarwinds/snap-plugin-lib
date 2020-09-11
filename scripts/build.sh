3fd91b61eef74f525b25dd112c5b4067542a2d1a
Tomasz Kolodziej <tomasz.kolodziej@solarwinds.cloud>
2020-09-11 10:08:05 +0200

    * better script

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,25 +23,29 @@ set -o pipefail
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __proj_dir="$(dirname "$__dir")"
 
-version=${TRAVIS_TAG:-250.0.1}
-readarray -d . -t versionarr < <(printf '%s' ${version})
+version=${TRAVIS_TAG:-250.0.0}
+versionarr=($(echo $version | tr "." "\n"))
 commit=${TRAVIS_COMMIT:-0}
 
 # shellcheck source=scripts/common.sh
 . "${__dir}/common.sh"
 
+pushd "${__proj_dir}/v2/bindings"
 # Build CGO for linux
 _info "Building swisnap-plugin-lib.so"
-(export GOOS=linux && cd "${__proj_dir}/v2/bindings" && _go_build "--buildmode=c-shared" "swisnap-plugin-lib.so")
+(export GOOS=linux && _go_build "--buildmode=c-shared" "swisnap-plugin-lib.so")
 
 # Build CGO for windows 
 _info "Building swisnap-plugin-lib.dll"
 _go_get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
-(cd "${__proj_dir}/v2/bindings" && cp versioninfo.template versioninfo.json)
-(cd "${__proj_dir}/v2/bindings" && sed -i 's/{major_version}/'${versionarr[0]}'/g' versioninfo.json)
-(cd "${__proj_dir}/v2/bindings" && sed -i 's/{minor_version}/'${versionarr[1]}'/g' versioninfo.json)
-(cd "${__proj_dir}/v2/bindings" && sed -i 's/{patch_version}/'${versionarr[2]}'/g' versioninfo.json)
-(cd "${__proj_dir}/v2/bindings" && sed -i 's/{version}/'${version}'/g' versioninfo.json)
-(cd "${__proj_dir}/v2/bindings" && sed -i 's/$TRAVIS_COMMIT/'${commit}'/g' versioninfo.json)
-(cd "${__proj_dir}/v2/bindings" && go generate)
-(export GOOS=windows && export CGO_ENABLED=1 && export CXX=x86_64-w64-mingw32-g++ && export CC=x86_64-w64-mingw32-gcc && cd "${__proj_dir}/v2/bindings" && _go_build "--buildmode=c-shared" "swisnap-plugin-lib.dll")
+cp versioninfo.template versioninfo.json
+sed -i 's/{major_version}/'${versionarr[0]}'/g' versioninfo.json
+sed -i 's/{minor_version}/'${versionarr[1]}'/g' versioninfo.json
+sed -i 's/{patch_version}/'${versionarr[2]}'/g' versioninfo.json
+sed -i 's/{version}/'${version}'/g' versioninfo.json
+sed -i 's/$TRAVIS_COMMIT/'${commit}'/g' versioninfo.json
+go generate
+(export GOOS=windows && export CGO_ENABLED=1 && export CXX=x86_64-w64-mingw32-g++ && export CC=x86_64-w64-mingw32-gcc  \
+ && _go_build "--buildmode=c-shared" "swisnap-plugin-lib.dll")
+
+popd
