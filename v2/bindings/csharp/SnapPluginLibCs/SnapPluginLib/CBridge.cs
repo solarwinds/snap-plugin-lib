f092de1f911e5aa6f5e3e6876979c5c73ea89c39
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2021-02-17 14:34:00 +0100

    Added os-depenetent layer to CBridge
    JIRA: AO-17842

--- v2/bindings/csharp/SnapPluginLibCs/SnapPluginLib/CBridge.cs
+++ v2/bindings/csharp/SnapPluginLibCs/SnapPluginLib/CBridge.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace SnapPluginLib
 {
@@ -8,75 +7,287 @@ namespace SnapPluginLib
      */
     internal static class CBridge
     {
-        #if _WINDOWS
-        private const string PluginLibDllName = "swisnap-plugin-lib.dll";
-        #else
-        private const string PluginLibDllName = "swisnap-plugin-lib.so";
-        #endif        
-
         // Runner
-        
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern void start_collector(
-            Runner.CollectHandler collectHandler,
-            Runner.LoadHandler loadHandler,
-            Runner.UnloadHandler unloadHandler,
-            Runner.DefineHandler defineHandler,
-            string name,
-            string version
-        );
-        
+
+        internal static void start_collector(
+            Runner.CollectHandler collectHandler, Runner.LoadHandler loadHandler, Runner.UnloadHandler unloadHandler,
+            Runner.DefineHandler defineHandler, string name, string version
+        )
+        {
+            if (IsWindows())
+            {
+                CBridgeWin.start_collector(collectHandler, loadHandler, unloadHandler, defineHandler, name, version);
+            }
+            else if (IsLinux())
+            {
+                CBridgeLinux.start_collector(collectHandler, loadHandler, unloadHandler, defineHandler, name, version);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
+
         // Collect context related functions
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr /* NativeError */
-            ctx_add_metric(string taskId, string ns, NativeValue nativeValue, NativeModifiers nativeModifiers);
+        internal static IntPtr /* NativeError */
+            ctx_add_metric(string taskId, string ns, NativeValue nativeValue, NativeModifiers nativeModifiers)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_add_metric(taskId, ns, nativeValue, nativeModifiers);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_add_metric(taskId, ns, nativeValue, nativeModifiers);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr /* NativeError */
-            ctx_always_apply(string taskId, string ns, NativeModifiers nativeModifiers);
+        internal static IntPtr /* NativeError */
+            ctx_always_apply(string taskId, string ns, NativeModifiers nativeModifiers)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_always_apply(taskId, ns, nativeModifiers);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_always_apply(taskId, ns, nativeModifiers);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern void ctx_dismiss_all_modifiers(string taskId);
+        internal static void ctx_dismiss_all_modifiers(string taskId)
+        {
+            if (IsWindows())
+            {
+                CBridgeWin.ctx_dismiss_all_modifiers(taskId);
+            }
+            else if (IsLinux())
+            {
+                CBridgeLinux.ctx_dismiss_all_modifiers(taskId);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern int ctx_should_process(string taskId, string ns);
+        internal static int ctx_should_process(string taskId, string ns)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_should_process(taskId, ns);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_should_process(taskId, ns);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr ctx_requested_metrics(string taskId);
+        internal static IntPtr ctx_requested_metrics(string taskId)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_requested_metrics(taskId);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_requested_metrics(taskId);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
         // Context related functions
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr ctx_config_value(string taskId, string key);
+        internal static IntPtr ctx_config_value(string taskId, string key)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_config_value(taskId, key);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_config_value(taskId, key);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr ctx_config_keys(string taskId);
+        internal static IntPtr ctx_config_keys(string taskId)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_config_keys(taskId);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_config_keys(taskId);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr ctx_raw_config(string taskId);
+        internal static IntPtr ctx_raw_config(string taskId)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_raw_config(taskId);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_raw_config(taskId);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern string ctx_add_warning(string taskId, string message);
+        internal static string ctx_add_warning(string taskId, string message)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_add_warning(taskId, message);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_add_warning(taskId, message);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern string ctx_log(string taskId, int level, string message, IntPtr /* NativeMap */ fields);
+        internal static string ctx_log(string taskId, int level, string message, IntPtr /* NativeMap */ fields)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.ctx_log(taskId, level, message, fields);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.ctx_log(taskId, level, message, fields);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
         // DefinePlugin related functions 
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern void define_metric(string ns, string unit, int idDefault, string description);
+        internal static void define_metric(string ns, string unit, int isDefault, string description)
+        {
+            if (IsWindows())
+            {
+                CBridgeWin.define_metric(ns, unit, isDefault, description);
+            }
+            else if (IsLinux())
+            {
+                CBridgeLinux.define_metric(ns, unit, isDefault, description);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern void define_group(string name, string description);
+        internal static void define_group(string name, string description)
+        {
+            if (IsWindows())
+            {
+                CBridgeWin.define_group(name, description);
+            }
+            else if (IsLinux())
+            {
+                CBridgeLinux.define_group(name, description);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern IntPtr /* NativeError */ define_example_config(string config);
+        internal static IntPtr /* NativeError */ define_example_config(string config)
+        {
+            if (IsWindows())
+            {
+                return CBridgeWin.define_example_config(config);
+            }
+            else if (IsLinux())
+            {
+                return CBridgeLinux.define_example_config(config);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern void define_tasks_per_instance_limit(int limit);
+        internal static void define_tasks_per_instance_limit(int limit)
+        {
+            if (IsWindows())
+            {
+                CBridgeWin.define_tasks_per_instance_limit(limit);
+            }
+            else if (IsLinux())
+            {
+                CBridgeLinux.define_tasks_per_instance_limit(limit);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
 
-        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
-        internal static extern void define_instances_limit(int limit);
+        internal static void define_instances_limit(int limit)
+        {
+            if (IsWindows())
+            {
+                CBridgeWin.define_instances_limit(limit);
+            }
+            else if (IsLinux())
+            {
+                CBridgeLinux.define_instances_limit(limit);
+            }
+            else
+            {
+                throw new NotImplementedException(NoImplementedError);
+            }
+        }
+        
+        private static bool IsWindows()
+        {
+            return Environment.OSVersion.Platform == PlatformID.Win32Windows
+                   || Environment.OSVersion.Platform == PlatformID.Win32NT;
+        }
+
+        private static bool IsLinux()
+        {
+            return Environment.OSVersion.Platform == PlatformID.Unix;
+        }
+
+        private static string NoImplementedError = $"no implementation for {Environment.OSVersion.Platform.ToString()}";
+
     }
 }
---
+++ v2/bindings/csharp/SnapPluginLibCs/SnapPluginLib/CBridgeLinux.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SnapPluginLib
+{
+    /*
+     * Responsible for calling all exported (native) C functions 
+     */
+    internal static class CBridgeLinux
+    {
+        private const string PluginLibDllName = "swisnap-plugin-lib.so";
+
+        // Runner
+        
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern void start_collector(
+            Runner.CollectHandler collectHandler,
+            Runner.LoadHandler loadHandler,
+            Runner.UnloadHandler unloadHandler,
+            Runner.DefineHandler defineHandler,
+            string name,
+            string version
+        );
+        
+        // Collect context related functions
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr /* NativeError */
+            ctx_add_metric(string taskId, string ns, NativeValue nativeValue, NativeModifiers nativeModifiers);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr /* NativeError */
+            ctx_always_apply(string taskId, string ns, NativeModifiers nativeModifiers);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern void ctx_dismiss_all_modifiers(string taskId);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern int ctx_should_process(string taskId, string ns);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr ctx_requested_metrics(string taskId);
+
+        // Context related functions
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr ctx_config_value(string taskId, string key);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr ctx_config_keys(string taskId);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr ctx_raw_config(string taskId);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern string ctx_add_warning(string taskId, string message);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern string ctx_log(string taskId, int level, string message, IntPtr /* NativeMap */ fields);
+
+        // DefinePlugin related functions 
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern void define_metric(string ns, string unit, int idDefault, string description);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern void define_group(string name, string description);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern IntPtr /* NativeError */ define_example_config(string config);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern void define_tasks_per_instance_limit(int limit);
+
+        [DllImport(PluginLibDllName, CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern void define_instances_limit(int limit);
+    }
+}
