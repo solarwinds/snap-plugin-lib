0895b55c56eb8b2a7df89db7d551c91b010468c1
lootek <piotr.lucinski@solarwinds.com>
2021-03-03 11:43:23 +0100

    copyright fix

--- v2/runner/collector.go
+++ v2/runner/collector.go
@@ -3,7 +3,7 @@ The package "runner" provides simple API to start plugins in different modes.
 */
 
 /*
- Copyright (c) 2020 SolarWinds Worldwide, LLC
+ Copyright (c) 2021 SolarWinds Worldwide, LLC
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
