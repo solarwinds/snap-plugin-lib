0c82f3ae79065d303d7965463fe4ca3165dfa018
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2021-08-11 15:18:59 +0200

    Example 10000 metrics

--- examples/v2/snap-plugin-collector-example/main.go
+++ examples/v2/snap-plugin-collector-example/main.go
@@ -19,11 +19,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
 
-	"github.com/sirupsen/logrus"
 	"github.com/solarwinds/snap-plugin-lib/v2/plugin"
 	"github.com/solarwinds/snap-plugin-lib/v2/runner"
 )
@@ -31,112 +27,20 @@ import (
 const (
 	pluginName    = "example"
 	pluginVersion = "0.0.1"
-
-	maxCollectDuration = 5 * time.Second
 )
 
-var log = logrus.WithFields(logrus.Fields{
-	"layer": "plugin",
-	"name":  pluginName,
-})
-
-var exampleConfig = `
----
-version: 2
-
-schedule:
-  type: simple
-  interval: "60s"
-
-plugins:
-  - plugin_name: example
-
-    config:
-        # Random value used to calculate metric4
-        crand: 40
-        
-        # other tree-like configuration
-        credentials:
-            user: admin
-            password: secure1
-            token: abcd-1234
-
-    publish:
-      - plugin_name: publisher-appoptics
-`
-
 type myCollector struct {
-	random1History map[int]int
-	random2History map[int16]int
-}
-
-func newMyCollector() *myCollector {
-	return &myCollector{
-		random1History: map[int]int{},
-		random2History: map[int16]int{},
-	}
-}
-
-func (*myCollector) PluginDefinition(def plugin.CollectorDefinition) error {
-	_ = def.DefineTasksPerInstanceLimit(5)
-	_ = def.DefineInstancesLimit(plugin.NoLimit)
-
-	def.DefineMetric("/example/static/random1", "", true, "Random value (0-10)")
-	def.DefineMetric("/example/static/random2", "", true, "Random value (0-20)")
-	def.DefineMetric("/example/global/random3", "", true, "Random value (0-50)")
-	def.DefineMetric("/example/config/random4", "", true, "Random value (0-crand)")
-
-	def.DefineMetric("/example/nodefault/random5", "", false, "Random value")
-
-	_ = def.DefineExampleConfig(exampleConfig)
-
-	return nil
 }
 
 func (c *myCollector) Collect(ctx plugin.CollectContext) error {
-	log.Trace("Collect executed")
-
-	// Simulate collector processing
-	time.Sleep(time.Duration(rand.Intn(int(maxCollectDuration)))) // #nosec G404
-
-	random1 := rand.Intn(10)        // #nosec G404
-	random2 := int16(rand.Intn(20)) // #nosec G404
-
-	c.random1History[random1]++
-	c.random2History[random2]++
-
-	_ = ctx.AddMetric("/example/static/random1", random1)
-	_ = ctx.AddMetric("/example/static/random2", random2)
-
-	globalRandom, _ := ctx.Load("random")
-	_ = ctx.AddMetric("/example/global/random3", globalRandom)
-
-	configRandom, ok := ctx.Load("random-config")
-	if ok {
-		n := rand.Intn(configRandom.(int)) // #nosec G404
-		_ = ctx.AddMetric("/example/config/random4", n, plugin.MetricTag("random", fmt.Sprintf("%v", configRandom)))
-	}
-
-	return nil
-}
-
-func (c *myCollector) CustomInfo(ctx plugin.Context) interface{} {
-	return map[string]interface{}{
-		"random1Hist": c.random1History,
-		"random2Hist": c.random2History,
-	}
-}
-
-func (*myCollector) Load(ctx plugin.Context) error {
-	log.Tracef("Load executed, configs=%s", ctx.ConfigKeys())
-
-	ctx.Store("random", rand.Intn(50)) // #nosec G404
-
-	v, ok := ctx.ConfigValue("crand")
-	if ok {
-		intV, err := strconv.Atoi(v)
-		if err == nil {
-			ctx.Store("random-config", intV)
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			ns := fmt.Sprintf("/example/group_%d/metric_%d", i, j)
+			tags := map[string]string{
+				"t1": fmt.Sprintf("%d", i),
+				"t2": fmt.Sprintf("%d", j),
+			}
+			_ = ctx.AddMetric(ns, i*j, plugin.MetricTags(tags))
 		}
 	}
 
@@ -144,5 +48,5 @@ func (*myCollector) Load(ctx plugin.Context) error {
 }
 
 func main() {
-	runner.StartCollectorWithContext(context.Background(), newMyCollector(), pluginName, pluginVersion)
+	runner.StartCollectorWithContext(context.Background(), &myCollector{}, "example", pluginVersion)
 }
