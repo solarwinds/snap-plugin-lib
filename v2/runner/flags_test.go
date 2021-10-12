f70422261ac5c4c23f6944d4a3b5389b1b847586
lootek <piotr.lucinski@solarwinds.com>
2021-10-12 13:15:05 +0200

    cleanup

--- v2/runner/flags_test.go
+++ v2/runner/flags_test.go
@@ -1,7 +1,7 @@
 // +build small
 
 /*
- Copyright (c) 2021cd -= SolarWinds Worldwide, LLC
+ Copyright (c) 2021 SolarWinds Worldwide, LLC
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
