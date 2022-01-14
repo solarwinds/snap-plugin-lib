6e8a9dcf03da9db033be57286388a8efaadb64de
lootek <piotr.lucinski@solarwinds.com>
2022-01-14 15:33:02 +0100

    satisfying bloody copyrights validator

--- examples/v1/snap-plugin-publisher-file/file/file.go
+++ examples/v1/snap-plugin-publisher-file/file/file.go
@@ -13,7 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 
- Copyright (c) 2021 SolarWinds Worldwide, LLC
+ Copyright (c) 2022 SolarWinds Worldwide, LLC
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
