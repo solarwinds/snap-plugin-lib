b967bad4fbfc98770948f6fa41146bc2d8755f6a
Maciej Poleszczyk <maciej.poleszczyk@solarwinds.com>
2022-05-25 14:37:45 +0200

    Update copyrights

--- v2/internal/plugins/common/proxy/context_test.go
+++ v2/internal/plugins/common/proxy/context_test.go
@@ -2,7 +2,7 @@
 // +build small
 
 /*
- Copyright (c) 2020 SolarWinds Worldwide, LLC
+ Copyright (c) 2022 SolarWinds Worldwide, LLC
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
