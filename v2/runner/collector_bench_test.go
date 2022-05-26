33805aab6d093b08d360520255e684126c5314db
poleszcz <maciej.poleszczyk@solarwinds.com>
2022-05-26 06:57:33 -0500

    Add GO 1.18.x (#146)
    JIRA: AO-21122

--- v2/runner/collector_bench_test.go
+++ v2/runner/collector_bench_test.go
@@ -1,3 +1,4 @@
+//go:build bench
 // +build bench
 
 /*
@@ -8,7 +9,7 @@ Benchmark test mainly measures:
 */
 
 /*
- Copyright (c) 2020 SolarWinds Worldwide, LLC
+ Copyright (c) 2022 SolarWinds Worldwide, LLC
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
