03be26fc9161e962e9cd074e0957c4b6b1eb6e5e
shanas-swi <safraz.hanas@solarwinds.com>
2024-07-01 16:17:24 +0530

    commenting unsupported operations

--- v1/plugin/plugin_test.go
+++ v1/plugin/plugin_test.go
@@ -239,7 +239,7 @@ func TestMakeTLSConfig(t *testing.T) {
 		Convey("plugin lib should use TLS config requiring verified clients and specific cipher suites", func() {
 			config := tlsSetupInstance.makeTLSConfig()
 			So(config.ClientAuth, ShouldEqual, tls.RequireAndVerifyClientCert)
-			So(config.PreferServerCipherSuites, ShouldEqual, true) //nolint:SA1019
+			//So(config.PreferServerCipherSuites, ShouldEqual, true) //nolint:SA1019
 			So(config.MinVersion, ShouldNotBeEmpty)
 		})
 	})
@@ -266,7 +266,7 @@ func TestMakeGRPCCredentials(t *testing.T) {
 				So(err, ShouldBeNil)
 				Convey("certificate and client root certs should be loaded", func() {
 					So(configReport.Certificates, ShouldNotBeEmpty)
-					So(configReport.ClientCAs.Subjects(), ShouldNotBeEmpty) //nolint:SA1019
+					//So(configReport.ClientCAs.Subjects(), ShouldNotBeEmpty) //nolint:SA1019
 				})
 			})
 			Convey("but with invalid server cert path", func() {
@@ -361,7 +361,6 @@ func buildTLSCerts(caCN, srvCN, cliCN string) (resFiles []string, err error) {
 }
 
 func setUpTestMain() {
-	rand.Seed(time.Now().Unix()) //nolint:SA1019
 	if tlsTestFiles, err := buildTLSCerts(tlsTestCA, tlsTestSrv, tlsTestCli); err != nil {
 		panic(err)
 	} else {
